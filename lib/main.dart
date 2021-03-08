bffb7e703be50f58b17ae3d2ef7087815175fdc3
Volkan <softwarengineer2@users.noreply.github.com>
2021-03-08 19:55:03 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:bloc_architecture_app/logic/bloc/weather/weather_bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'core/constants/strings.dart';
+import 'core/themes/app_theme.dart';
+import 'logic/debug/app_bloc_observer.dart';
+import 'presentation/router/app_router.dart';
+
+void main() {
+  Bloc.observer = AppBlocObserver();
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => WeatherBloc(),
+        ),
+      ],
+      child: MaterialApp(
+        title: Strings.appTitle,
+        theme: AppTheme.lightTheme,
+        darkTheme: AppTheme.darkTheme,
+        debugShowCheckedModeBanner: false,
+        initialRoute: AppRouter.home,
+        onGenerateRoute: AppRouter.onGenerateRoute,
+      ),
+    );
+  }
+}
