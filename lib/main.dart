bcf10075773942a6f936c81ee83441d610086f19
Volkan <softwarengineer2@users.noreply.github.com>
2021-03-08 20:38:11 +0100

    naming changed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:bloc_architecture_app/logic/bloc/weather/weather_bloc.dart';
+import 'logic/bloc/weather/weather_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
